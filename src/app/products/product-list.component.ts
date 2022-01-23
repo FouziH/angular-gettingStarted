e2dfde7bcd8a4e3135459c69e4ca03152e22ca58
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-23 08:37:40 -0600

    ....

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,9 +1,41 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
 })
 export class ProductListComponent {
-    pageTitle: String = "Product List";
+  pageTitle: String = 'Product List';
+  products: any[] = [
+    {
+      productId: 2,
+      productName: 'Garden Cart',
+      ProductCode: 'GDN-0023',
+      releaseDate: 'March 18, 2021',
+      description: "15 gallon capacity rolling garden cart",
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/image/garden_cart.png'
+    },
+    {
+      productId: 5,
+      productName: 'Garden Cart',
+      ProductCode: 'GDN-0023',
+      releaseDate: 'March 18, 2021',
+      description: "15 gallon capacity rolling garden cart",
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/image/garden_cart.png'
+    },
+    {
+      productId: 2,
+      productName: 'Garden Cart',
+      ProductCode: 'GDN-0023',
+      releaseDate: 'March 18, 2021',
+      description: "15 gallon capacity rolling garden cart",
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/image/garden_cart.png'
+    },
+  ];
 }
