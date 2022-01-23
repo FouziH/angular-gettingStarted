8b4a9e5f1a89ffe46114b3f16cc1cce9c5dc7d3e
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-23 10:19:48 -0600

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [
+  declarations:[
     AppComponent,
     ProductListComponent
   ],
