50877b2dc636cc6746719321da49940ebe61b7b9
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-24 15:13:12 -0600

    ...

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
 
 @Component({
-
+    selector: 'pm-star',
+    templateUrl: './star.component.html', 
+    styleUrls: ['./star.component.css']
 })
 
 export class StarComponent {
-
+    rating: number = 0;
+    cropWidth: number = 0;
 }
