5f823c296cc730d8ad8f816a85d920cbebe69165
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-24 07:38:21 -0600

    ....

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 
+
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
---
+++ src/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  productId: number;
+  productName: String;
+  productCode: String;
+  releaseDate: String;
+  Price: number;
+  Description: String;
+  starRating: number;
+  imageUrl: String;
+}
