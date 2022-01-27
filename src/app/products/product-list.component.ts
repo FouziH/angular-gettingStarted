698263b45f33c58b6c17357cd004526c9c44cba1
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-27 05:40:12 -0600

    ...

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
@@ -7,12 +8,14 @@ import { ProductService } from './product.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
   errorMessage ='';
+
+  sub!: Subscription ;
   //   listFilter: string = 'cart';
   //   count: number =0;
 
@@ -51,18 +54,28 @@ export class ProductListComponent implements OnInit {
   //on Init is similar to useEffect hook in react
   ngOnInit(): void {
     console.log('In OnInit');
-    this.sub = this.productService.getProducts().subscribe({
-      next: this.products => this.products = this.products, 
+    this.sub =  this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+         this.filteredProducts = this.products;
+      
+      }, 
       error: err => this.errorMessage = err
     });
-    this.filteredProducts = this.products;
+   
     // this.listFilter = '';
   }
   onRatingClicked(message: string):void {
     this.pageTitle = 'Product List: ' + message;
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+  // 
+
+  ngOnDestroy(){
+    this.sub.unsubscribe()
   }
+  
+
+
+
 }
