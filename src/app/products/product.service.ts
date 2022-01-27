7e38b1a962c8239b44302621cfb86528cef79262
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-27 05:07:36 -0600

    ...

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from "@angular/core";
-import { IProduct } from "./product";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {catchError, tap} from 'rxjs/operators'
+import { IProduct } from './product';
 
 @Injectable({
-    providedIn:"root"
+  providedIn: 'root',
 })
 export class ProductService {
-private productUrl = 'api/products/products.json'
-  constructor(private http: HttpClient){
+  private productUrl = 'api/products/products.json';
+  constructor(private http: HttpClient) {}
 
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All', JSON.stringify(data)))
+    );
   }
 
-    getProducts(): IProduct[] {
-        return this.http.get(this.productUrl)
-    }
+  private
 }
