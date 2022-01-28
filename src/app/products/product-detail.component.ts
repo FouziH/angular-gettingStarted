655e8ad61a02fc359316d2e60b693541d9ccbf06
unknown <hamzafowzi1@gmail.com>
2022-01-27 20:44:43 -0600

    ...

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-pagTitle: string = 'Product Details';
+pageTitle: string = 'Product Details';
   constructor() { }
 
   ngOnInit(): void {
