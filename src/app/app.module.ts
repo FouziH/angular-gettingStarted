ee96c5d7d9eba77b1dcd4aef948d30e35003c7ed
hamza mohamed <hamzafowzi1@gmail.com>
2022-01-29 11:13:19 -0600

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import {RouterModule} from '@angular/router'
-import { ProductDetailGuard } from './products/product-detail.guard';
+
 import { ProductModule } from './products/product.module';
 
 @NgModule({
